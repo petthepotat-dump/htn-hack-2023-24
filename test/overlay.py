ee830e79b52a65bad5db3fdbd2dfeeffe452f7a1
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2023-09-16 04:04:57 -0400

    fewf

--- test/overlay.py
+++ test/overlay.py
@@ -1,31 +1,59 @@
 import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPainter, QBrush, QColor
 
-from PyQt5 import QtGui, QtCore, uic
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication
+
+# get monitor res
+from screeninfo import get_monitors
 
 
-class MainWindow(QMainWindow):
+class OverlayWidget(QWidget):
     def __init__(self):
-        QMainWindow.__init__(self)
-        self.setWindowFlags(
-            QtCore.Qt.WindowStaysOnTopHint |
-            QtCore.Qt.FramelessWindowHint |
-            QtCore.Qt.X11BypassWindowManagerHint
-        )
-        self.setGeometry(
-            QtWidgets.QStyle.alignedRect(
-                QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
-                QtCore.QSize(220, 32),
-                QtWidgets.qApp.desktop().availableGeometry()
-        ))
+        super().__init__()
+
+        # Set up the window
+        self.setWindowTitle("Overlay Window")
+
+        # ------- #
+        # only get main monitor
+        main = get_monitors()[0]
+        self.setGeometry(-20, 0, main.width, main.height)
+        print(main)
+
+        # Set the window's flags to make it act as an overlay
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+        self.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
+
+        self.rectangles = [(0, 0, 20, 20), (main.width - 20, main.width-100, 20, 20)]
+
+        painter = QPainter(self)
+        painter.setBrush(QBrush(QColor(255, 0, 0)))  # Red color
+
+        for rect in self.rectangles:
+            painter.drawRect(*rect)
+    
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)  # Start the painting session
+
+        painter.setBrush(QBrush(QColor(255, 0, 0)))  # Red color
+
+        for rect in self.rectangles:
+            painter.drawRect(*rect)
+
+        painter.end()  # End the painting session
+
 
     def mousePressEvent(self, event):
-        QtWidgets.qApp.quit()
+        # Close the overlay when clicked
+        # self.close()
+        pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mywindow = MainWindow()
-    mywindow.show()
-    app.exec_()
+    window = OverlayWidget()
+    window.show()
+    sys.exit(app.exec())
+
--- test5.py
+++ test5.py
@@ -1,2 +1,29 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
 
+class TransparentOverlay(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Set the window size and position
+        screen_geometry = QApplication.desktop().screenGeometry()
+        self.setGeometry(screen_geometry)
+
+        # Set window transparency
+        self.setWindowOpacity(0.3)
+
+        # Remove window decorations
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+        # Make the window stay always on top
+        self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+
+        # Set the window to not accept mouse events
+        self.setAttribute(Qt.WA_TransparentForMouseEvents)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    overlay = TransparentOverlay()
+    overlay.show()
+    sys.exit(app.exec_())
